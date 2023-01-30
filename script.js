a9cd47094e11d066552cce5f9076496f9039d512
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-01-30 11:57:36 -0300

    enviar ok

---
+++ script.js
@@ -0,0 +1,8 @@
+let descricaoAdd;
+
+let enviar = document.querySelector("#enviar").addEventListener('click', enviarDesc);
+
+function enviarDesc(){
+   let descricaoAdd = document.querySelector("#inputDesc").value;
+   alert(descricaoAdd);
+};
