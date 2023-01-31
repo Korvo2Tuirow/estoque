9c70a179f5935474d21cf8da931a0f3e1facc0bb
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-01-31 15:49:27 -0300

    import poo

---
+++ poo.js
@@ -0,0 +1,18 @@
+export class Produto{
+    constructor(descricao, quantidade, valor, valorTotal){
+      this._descricao = descricao;
+      this._quantidade = quantidade;
+      this._valor = valor;
+      this._valorTotal = this._quantidade * this._valor;
+    };
+    
+    set (descricao, quantidade, valor){
+      return this._descricao = descricao,
+             this._quantidade = quantidade,
+             this._valor = this._quantidade * this._valor;
+    };
+    totalValor(){
+      return (this._quantidade * this._valor).toFixed(2);
+    }
+    
+  };
--- script.js
+++ script.js
@@ -1,26 +1,11 @@
+import { Produto } from "./poo";
+
 let descricaoAdd;
 let quantidadeAdd;
 let valorAdd;
 let p1;
 
-class Produto{
-  constructor(descricao, quantidade, valor, valorTotal){
-    this._descricao = descricao;
-    this._quantidade = quantidade;
-    this._valor = valor;
-    this._valorTotal = this._quantidade * this._valor;
-  };
-  
-  set (descricao, quantidade, valor){
-    return this._descricao = descricao,
-           this._quantidade = quantidade,
-           this._valor = this._quantidade * this._valor;
-  };
-  totalValor(){
-    return (this._quantidade * this._valor).toFixed(2);
-  }
-  
-};
+
 
 let enviar = document.querySelector("#enviar").addEventListener('click', enviarDesc);
 
