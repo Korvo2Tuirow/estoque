be5c0af1cdd24a40947290631da9c5d49340df0a
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-01-31 10:11:59 -0300

    teste

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 let descricaoAdd;
+let quantidadeAdd;
 let valorAdd;
-let quantAdd;
+let p1;
+
+class Produto{
+  constructor(descricao, quantidade, valor, valorTotal){
+    this._descricao = descricao;
+    this._quantidade = quantidade;
+    this._valor = valor;
+    this._valorTotal = this._quantidade * this._valor;
+  };
+  
+  set (descricao, quantidade, valor){
+    return this._descricao = descricao,
+           this._quantidade = quantidade,
+           this._valor = totalValor();
+  };
+  totalValor(){
+    return (this._quantidade * this._valor).toFixed(2);
+  }
+  
+};
 
 let enviar = document.querySelector("#enviar").addEventListener('click', enviarDesc);
 
 function enviarDesc(){
    descricaoAdd = document.querySelector("#inputDesc").value;
+   quantidadeAdd = document.querySelector("#inputQuant").value;
    valorAdd = document.querySelector("#inputValor").value;
-   quantAdd = document.querySelector("#inputQuant").value;
-   alert(descricaoAdd + valorAdd + quantAdd);
+   
+   p1 = new Produto(descricaoAdd, quantidadeAdd, valorAdd);
+  console.log(p1);
 };
