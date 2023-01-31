174b47a2a1acfbde2a872726fbe841ed0eeaac09
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-01-31 16:20:34 -0300

    teste

--- script.js
+++ script.js
@@ -20,5 +20,5 @@ function enviarDesc(){
   let d1 = document.querySelector("#d1").innerHTML = p1._descricao;
   let q1 = document.querySelector("#q1").innerHTML = p1._quantidade;
   let vu1 = document.querySelector("#vu").innerHTML = p1._valor;
-  let vt = document.querySelector("#vt").innerHTML = p1._valorTotal;
+  let vt = document.querySelector("#vt").innerHTML = p1.totalValor();
 };
