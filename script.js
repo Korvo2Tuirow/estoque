848f48aab6a54382f2609bcb13670753600a83f6
Korvo2Tuirow <korvo2irow@live.com>
2023-02-13 03:29:14 -0300

    mudanças css html

--- script.js
+++ script.js
@@ -6,17 +6,7 @@ document.querySelector("#enviar").addEventListener("click", enviarDesc);
 
 function enviarDesc() {
  // preventDefault();
-  descricaoAdd = document.querySelector("#inputDesc").value;
-  quantidadeAdd = document.querySelector("#inputQuant").value;
-  valorAdd = document.querySelector("#inputValor").value;
-
-  new Produto(descricaoAdd, quantidadeAdd, valorAdd);
-
-  d1 = document.querySelector("#d1").innerHTML = p1._descricao;
-  q1 = document.querySelector("#q1").innerHTML = p1._quantidade;
-  vu1 = document.querySelector("#vu").innerHTML = p1._valor;
-  vt = document.querySelector("#vt").innerHTML = p1._valorTotal;
-  console.log(p1);
+  
 
 
 };
